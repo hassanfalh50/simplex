c86302f3e499576060a9852c79b42eff1b17a248
hassanfalh50 <89648962+hassanfalh50@users.noreply.github.com>
2023-05-13 23:26:46 +0300

    Create ss.js

---
+++ ss.js
@@ -0,0 +1,42 @@
+  $(document).ready(function () {
+    const _token = '';
+    const getDownloadUrl = '';
+    var waitingTime = 5;
+
+    function prepareDownloadLinkCountDown() {
+      var td = $("#material-download-waiting-time");
+      td.html(`${waitingTime--}`);
+
+      if (waitingTime >= 0)
+        setTimeout(prepareDownloadLinkCountDown, 1000);
+
+      if (waitingTime == -1) {
+        setTimeout(() => {
+          $("#material-download-waiting").html(`<p class="text-center">تم تجهيز رابط التحميل ... يرجى الانتظار</p>`);
+
+          setTimeout(() => {
+            const params = {
+              _token: _token
+            };
+
+            $.post(getDownloadUrl, params, function (data, textStatus, jqXHR) {
+              if (textStatus == 'success') {
+                var a = data.a;
+                var name = data.name;
+
+                var form = `
+                  <form id="material-download-form" method="get" target="_blank">
+                    <a href="${a}" class="btn btn-success d-block mx-auto downlaod-file-msgs big-btn" type="submit">بدء التحميل</a>
+                  </form>
+                `;
+                $("#material-download-waiting").fadeOut();
+                $("#material-download-waiting").parent().append(form);
+              }
+            });
+          }, 1000);
+        }, 1000);
+      }
+    }
+
+    prepareDownloadLinkCountDown();
+  });
